a8fee4f4d04b4c5584ff546818d12b2b6d9748f5
LingDev <lingjing0921@live.com>
2023-02-20 00:06:43 +0800

    Fix 'Add-Migration' error (#4)

--- src/Ling.EntityFrameworkCore/Abstractions/IEntity.cs
+++ src/Ling.EntityFrameworkCore/Abstractions/IEntity.cs
@@ -1,4 +1,4 @@
-﻿namespace LingDev.EntityFrameworkCore.Abstractions;
+﻿namespace Ling.EntityFrameworkCore.Abstractions;
 
 /// <summary>
 /// Represents an entity. Its' primary key may not be "Id" or it may have a composite primary key.
